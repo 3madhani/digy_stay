ab07927d11ce45e152ed887cde3c0371178c93f7
3mad Hany <144627820+3madhani@users.noreply.github.com>
2025-05-05 05:18:21 +0300

    feat: Update back navigation in AppBar to redirect to OnBoardingView

--- lib/core/common/custom_app_bar.dart
+++ lib/core/common/custom_app_bar.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
+import '../../feature/onboarding/presentation/views/onboarding_view.dart';
 import '../utils/app_text_styles.dart';
 
 AppBar buildAppBar({required BuildContext context, required String title}) {
   return AppBar(
     backgroundColor: Colors.white,
     leading: GestureDetector(
-      onTap: () => Navigator.pop(context),
+      onTap:
+          () =>
+              Navigator.pushReplacementNamed(context, OnBoardingView.routeName),
       child: const Icon(Icons.arrow_back_ios_new),
     ),
     centerTitle: true,
